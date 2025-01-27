23b87b127e1f7bdf00dbe5ad0da3622054ff0624
charlieroth <charlieroth4@gmail.com>
2025-01-27 16:28:49 +0100

    ratatui counter app example

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,96 @@
-use clap::Parser;
-mod app;
-mod cli;
-mod config;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+use std::io;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let config = config::Config::load("config.yaml")?;
-    let app = app::App::new(config);
-    app.connect().await?;
+#[derive(Debug, Default)]
+struct App {
+    counter: u8,
+    exit: bool,
+}
 
-    let args = cli::CliArgs::parse();
-    match args.cmd {
-        cli::CliCommand::Post { message } => match app.publish_text_note(&message).await {
-            Ok(event_id) => {
-                println!("Posted message with event id: {}", event_id);
-                Ok(())
-            }
-            Err(e) => anyhow::bail!(e),
-        },
-        cli::CliCommand::Profile { npub } => match app.profile(npub).await {
-            Ok(profile) => {
-                println!("{:?}", profile);
-                Ok(())
-            }
-            Err(e) => anyhow::bail!(e),
-        },
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
     }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)?;
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> io::Result<()> {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Counter ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
